2d1cbbf7767ffbd31ae74402c9d1c7c167d85a87
arcticmaticwebsite <93982819+IrinaSkrytska@users.noreply.github.com>
2025-02-24 22:27:33 +0200

    adds coat of arms

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -5,11 +5,18 @@ import css from "./Hero.module.css";
 import Logo from "../../images/Logo.png";
 import Phone from "@/app/images/icons/Phone";
 import Header from "../Header/Header";
+import CoatOfArms from "../../images/CoatOfArms.png";
 
 export default function Hero() {
   return (
     <section className={css.heroSection}>
       <Header />
+      <Image
+        className={css.coatOfArmsIcon}
+        width={66}
+        height={100}
+        src={CoatOfArms}
+      />
       <div className={css.heroThumb}>
         <p className={css.heroTitle}>
           національна асоціація правозахисників та адвокатів україни
